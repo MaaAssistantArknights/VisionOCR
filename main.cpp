2dc58c23f0b926ea75beb42fc665c4bf3eb5bd16
Hao Guan <10684225+hguandl@users.noreply.github.com>
2022-10-26 00:04:44 +1000

    fix: resize small images

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
 #include "VisionOCR.h"
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char* argv[]) {
     if (argc != 2) {
         std::cerr << "Please enter the filename" << std::endl;
         return 1;
@@ -19,38 +19,51 @@ int main(int argc, const char * argv[]) {
     cv::Mat image = cv::imread(argv[1]);
 
     constexpr size_t MaxBoxSize = 256;
+    constexpr size_t MaxTextSize = 4096;
 
     auto ocr = PaddleOcrCreate("", "", "", nullptr);
 
-    int boxes_buffer[MaxBoxSize * 8] = { 0 };
-    char* strs_buffer[MaxBoxSize] = { nullptr };
-    float scores_buffer[MaxBoxSize] = { 0 };
+    int boxes_buffer[MaxBoxSize * 8] = {0};
+    char* strs_buffer[MaxBoxSize] = {nullptr};
+    float scores_buffer[MaxBoxSize] = {0};
 
     size_t size = 0;
+
+    for (size_t i = 0; i != MaxBoxSize; ++i) {
+        strs_buffer[i] = new char[MaxTextSize];
+    }
 
     std::vector<uchar> buf;
     cv::imencode(".png", image, buf);
 
-    auto result = PaddleOcrSystem(ocr, buf.data(), buf.size(), false, boxes_buffer, strs_buffer, scores_buffer, &size, nullptr, nullptr);
+    auto result =
+        PaddleOcrSystem(ocr, buf.data(), buf.size(), false, boxes_buffer,
+                        strs_buffer, scores_buffer, &size, nullptr, nullptr);
 
     if (result == OCR_SUCCESS) {
         for (auto i = 0u; i < size; i++) {
-            std::cout
-            << i << "\t"
-            << "det boxes: ["
-            << "[" << boxes_buffer[i * 8] << "," << boxes_buffer[i * 8 + 1] << "]"
-            << ","
-            << "[" << boxes_buffer[i * 8 + 2] << "," << boxes_buffer[i * 8 + 3] << "]"
-            << ","
-            << "[" << boxes_buffer[i * 8 + 4] << "," << boxes_buffer[i * 8 + 5] << "]"
-            << ","
-            << "[" << boxes_buffer[i * 8 + 6] << "," << boxes_buffer[i * 8 + 7] << "]"
-            << "] rec text: " << strs_buffer[i]
-            << " rec score: " << scores_buffer[i]
-            << std::endl;
+            std::cout << i << "\t"
+                      << "det boxes: ["
+                      << "[" << boxes_buffer[i * 8] << ","
+                      << boxes_buffer[i * 8 + 1] << "]"
+                      << ","
+                      << "[" << boxes_buffer[i * 8 + 2] << ","
+                      << boxes_buffer[i * 8 + 3] << "]"
+                      << ","
+                      << "[" << boxes_buffer[i * 8 + 4] << ","
+                      << boxes_buffer[i * 8 + 5] << "]"
+                      << ","
+                      << "[" << boxes_buffer[i * 8 + 6] << ","
+                      << boxes_buffer[i * 8 + 7] << "]"
+                      << "] rec text: " << strs_buffer[i]
+                      << " rec score: " << scores_buffer[i] << std::endl;
         }
     } else {
         std::cerr << "OCR Failed." << std::endl;
+    }
+
+    for (size_t i = 0; i != MaxBoxSize; ++i) {
+        delete[] strs_buffer[i];
     }
 
     PaddleOcrDestroy(ocr);
